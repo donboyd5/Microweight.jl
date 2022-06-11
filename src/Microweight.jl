0ad4983491db417ec23114e48ac55232cd3bd325
Don Boyd <donboyd5@gmail.com>
2022-06-11 05:57:24 -0400

    add lsq to mw

--- src/Microweight.jl
+++ src/Microweight.jl
@@ -1,11 +1,16 @@
 module Microweight
 
 # using Revise  # in REPL
+using LsqFit
 
 # export f, g
-export geo_targets, geo_weights, objfn, sspd
+export geo_targets, geo_weights, objfn, sspd, lsq
 
 include("functions_poisson.jl")
 include("functions_poisson_fg.jl")
+
+function lsq(ibeta, wh, xmat, geotargets)
+  LsqFit.lmfit(f, ibeta, Float64[]; autodiff=:forwarddiff, show_trace=true, maxIter=50)
+end
 
 end
