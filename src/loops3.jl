7474ec6df8fc92b77d4dfbf5349d9887688b2b50
Don Boyd <donboyd5@gmail.com>
2022-06-22 10:59:14 -0400

    better looping

--- src/loops3.jl
+++ src/loops3.jl
@@ -1,61 +1,67 @@
 module loop
 
-function whs3(wh, mshares, h, s, whs)
+global fcalls
+
+function whs(wh, mshares, p)
     # note that mshares = reshape(shares, length(wh), :)
     # same as whs = wh .* mshares
     # whs h, s is preallocated
-    for i in 1:h
-        for j in 1:s
-            whs[i, j] = wh[i] * mshares[i, j]
+
+    for i in 1:p.h
+        for j in 1:p.s
+            p.whs[i, j] = wh[i] * mshares[i, j]
         end
     end
-    whs
+    p.whs
 end
 
-function calctargets3(whs, xmat, h, k, s, calctargets)
+function calctargets(whs, xmat, p)
     # same as calctargets = whs' * xmat
     # calctargets s, k is preallocated
-    for is in 1:s
-        for ik in 1:k
-            for ih in 1:h
-                calctargets[is, ik] = calctargets[is, ik] + whs[ih, is] * xmat[ih, ik]
+    p.calctargets = zeros(p.s, p.k)
+    for is in 1:p.s
+        for ik in 1:p.k
+            for ih in 1:p.h
+                p.calctargets[is, ik] = p.calctargets[is, ik] + whs[ih, is] * xmat[ih, ik]
             end
         end
     end
-    calctargets
+    p.calctargets
 end
 
-function pdiffs3(calctargets, geotargets, k, s, pdiffs)
+function pdiffs(calctargets, geotargets, p)
     # pdiffs s, k is preallocated
-    for is in 1:s
-        for ik in 1:k
-        pdiffs[is, ik] = (calctargets[is, ik] - geotargets[is, ik]) / geotargets[is, ik] * 100.
+    p.pdiffs = zeros(p.s, p.k)
+    for is in 1:p.s
+        for ik in 1:p.k
+            p.pdiffs[is, ik] = (calctargets[is, ik] - geotargets[is, ik]) / geotargets[is, ik] * 100.
         end
     end
-    pdiffs
+    p.pdiffs
 end
 
-function sspdfn3(pdiffs, k, s)
-    sspd = 0.0
-    for is in 1:s
-      for ik in 1:k
-        sspd = sspd + pdiffs[is, ik]^2
+function sspdfn(pdiffs, p)
+    p.sspd = 0.0
+    for is in 1:p.s
+      for ik in 1:p.k
+        p.sspd = p.sspd + pdiffs[is, ik]^2
       end
     end
-    sspd
+    p.sspd
 end
 
-function fopt3(shares, wh, xmat, geotargets, h, k, s, mshares, whs, calctargets, pdiffs)
+function fopt(shares, wh, xmat, geotargets, p)
     # shares: vec of (h * s)
     # wh, xmat, and geotargets are constants
     # mshares, whs, calctargets, and pdiffs are mutable but preallocated
-
-    mshares = reshape(shares, h, s) # shares, as a matrix
-    whs = whs3(wh, mshares, h, s, whs)
-    calctargets = calctargets3(whs, xmat, h, k, s, calctargets)
-    pdiffs = pdiffs3(calctargets, geotargets, k, s, pdiffs)
-    sspd = sspdfn3(pdiffs, k, s)
-    return sspd
+    # global fcalls += 1
+    p.mshares = reshape(shares, p.h, p.s)
+    p.whs = whs(wh, p.mshares, p)
+    p.calctargets = calctargets(p.whs, xmat, p)
+    p.pdiffs = pdiffs(p.calctargets, geotargets, p)
+    p.sspd = sspdfn(p.pdiffs, p)
+    # println("fcall: ", fcalls, " sspd: ", p.sspd)
+    return p.sspd
 end
 
 end
